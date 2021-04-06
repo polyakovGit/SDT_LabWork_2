c9ce38e7d5583403244378aaa31307c30eb61407
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 23:37:48 +0500

    в деструкторе не работает метод показа удаляемого элемента

--- lab2/Source.cpp
+++ lab2/Source.cpp
@@ -33,7 +33,14 @@ public:
 		this->name = name;
 		Add();
 	}
-
+	virtual ~Vehicle() {
+		while (head) {
+			Node* temp = head;
+			head = head->GetNext();
+			temp->GetVehicle().Show();
+			delete temp;
+		}
+	}
 	void Add() {
 		if (!head) {
 			head = tail = new Node(*this);
@@ -72,17 +79,50 @@ void Vehicle::Print() {
 }
 
 class Car : public Vehicle {
+	std::string engine;
 public:
-	Car(std::string modeltype) : Vehicle(modeltype) {}
-	void Show()const override{
-		std::cout << name<< "\n";
+	Car(std::string modeltype, std::string engine) : Vehicle(modeltype) {
+		this->engine = engine;
+	}
+
+	void Show()const {
+		std::cout << "Car: " << name << '\t' << engine << '\n';
+	}
+};
+
+class Train :public Vehicle {
+protected:
+	int capacity;
+public:
+	Train(std::string modelType, int capacity) : Vehicle(modelType) {
+		this->capacity = capacity;
+	}
+
+	void Show()const {
+		std::cout << "Train: " << name << '\t' << capacity << '\n';
+	}
+};
+
+class Express :public Train {
+	int speed;
+public:
+	Express(std::string modelType, int capacity, int speed) : Train(modelType, capacity) {
+		this->speed = speed;
+	}
+
+	void Show()const {
+		std::cout <<"Express: " << name << '\t' << capacity << '\t' << speed<<'\n';
 	}
 };
 
 int main() {
-	Car obj("test");
-	obj.Add();
-	Car obj2("test2");
-	Vehicle::Print();
+	//Car obj0("test0", "eng0");
+	//obj0.Add();
+	//Car obj1("test1", "eng1");
+	//Train obj2("test2", 15);
+	//obj2.Add();
+	Express obj3("Test3", 20, 400);
+	Car obj4("test4", "eng4");
+	//Vehicle::Print();
 	return 0;
 }
