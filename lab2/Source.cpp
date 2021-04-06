6c7c9ebc2f17a745f579e20b7557eb100c39bcd3
BuildTools <unconfigured@null.spigotmc.org>
2021-04-07 04:49:10 +0500

    раскидать по файлам

--- lab2/Source.cpp
+++ lab2/Source.cpp
@@ -117,13 +117,15 @@ public:
 };
 
 int main() {
-	//Car obj0("test0", "eng0");
-	//obj0.Add();
-	//Car obj1("test1", "eng1");
+	Car obj0("test0", "eng0");
+	obj0.Add();
+	Car obj1("test1", "eng1");
 	Train obj2("test2", 15);
-	//obj2.Add();
+	obj2.Add();
 	Express* obj3=new Express("Test3", 20, 400);
-	//Car obj4("test4", "eng4");
+	Car obj4("test4", "eng4");
+	obj3->Add();
+	
 	Vehicle::Print();
 	return 0;
 }
